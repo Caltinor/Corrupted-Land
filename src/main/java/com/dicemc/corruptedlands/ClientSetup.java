f9baabb3c9359bb16115a8662f241a62020cdd01
Caltinor <62700786+Caltinor@users.noreply.github.com>
2020-10-20 09:48:08 -0400

    improved grass

---
+++ src/main/java/com/dicemc/corruptedlands/ClientSetup.java
@@ -0,0 +1,11 @@
+package com.dicemc.corruptedlands;
+
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+public class ClientSetup {
+	public static void init(final FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(Registration.CORRUPTED_GRASS_BLOCK.get(), RenderType.getCutoutMipped());
+	}
+}
