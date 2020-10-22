63093f895047c5d051ba1281e1984273305f224d
Caltinor <62700786+Caltinor@users.noreply.github.com>
2020-10-22 09:41:20 -0400

    unified blocks via interface

--- src/main/java/com/dicemc/corruptedlands/CorruptedBlock.java
+++ src/main/java/com/dicemc/corruptedlands/CorruptedBlock.java
@@ -6,10 +6,15 @@ import com.dicemc.corruptedlands.CorruptedLandMod.Core;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
-public class CorruptedBlock extends Block{
+public class CorruptedBlock extends Block implements ICorrupted{
 
 	protected CorruptedBlock(Properties builder) { super(builder);}
 
@@ -18,4 +23,11 @@ public class CorruptedBlock extends Block{
 		double threshold = random.nextDouble();
 		if (threshold >= Config.SPREAD_RATE.get()) Core.corruptNeighbors(pos, worldIn);	
 	}
+	
+	public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
+		if (!entityIn.isImmuneToFire() && entityIn instanceof LivingEntity && !EnchantmentHelper.hasFrostWalker((LivingEntity)entityIn) && this.equals(Registration.CORRUPTED_OBSIDIAN_BLOCK.get())) {
+			entityIn.attackEntityFrom(DamageSource.HOT_FLOOR, 1.0F);
+		}
+		super.onEntityWalk(worldIn, pos, entityIn);
+ 	}
 }
---
+++ src/main/java/com/dicemc/corruptedlands/ICorrupted.java
@@ -0,0 +1,5 @@
+package com.dicemc.corruptedlands;
+
+public interface ICorrupted {
+
+}
