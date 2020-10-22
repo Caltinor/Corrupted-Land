8fc05fb0e49d718df8bf2c61a53c5ecc7b1cfc7b
Caltinor <62700786+Caltinor@users.noreply.github.com>
2020-10-22 09:39:46 -0400

    added ice

--- src/main/java/com/dicemc/corruptedlands/ClientSetup.java
+++ src/main/java/com/dicemc/corruptedlands/ClientSetup.java
@@ -7,5 +7,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 public class ClientSetup {
 	public static void init(final FMLClientSetupEvent event) {
 		RenderTypeLookup.setRenderLayer(Registration.CORRUPTED_GRASS_BLOCK.get(), RenderType.getCutoutMipped());
+		RenderTypeLookup.setRenderLayer(Registration.CORRUPTED_ICE_BLOCK.get(), RenderType.getTranslucent());
 	}
 }
